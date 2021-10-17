071e50fa897acd94e2a2ba70146a51b7aa42d23f
xiaohh <1296522554@qq.com>
2021-10-17 21:57:53 +0800

    添加nav组件

--- src/App.vue
+++ src/App.vue
@@ -1,8 +1,19 @@
 <template>
   <div id="app">
     <router-view/>
+    <hr>
+    <Nav />
   </div>
 </template>
+<script lang="ts">
+import Nav from "@/components/Nav.vue";
+export default {
+  components:{
+    Nav,
+  }
+};
+</script>
+
 
 <style lang="scss">
 *{margin: 0;padding: 0;box-sizing: border-box;}
---
+++ src/components/Nav.vue
@@ -0,0 +1,50 @@
+<template>
+  <nav>
+    <div>
+      <svg class="icon" aria-hidden="true">
+        <use xlink:href="#icon-biaoqian"></use>
+      </svg>
+      <router-link to="/statistics">标签</router-link>
+    </div>
+    <div>
+      <svg class="icon" aria-hidden="true">
+        <use xlink:href="#icon-jizhang"></use>
+      </svg>
+      <router-link to="/money" class="item">记一笔</router-link>
+    </div>
+    <div>
+      <svg class="icon" aria-hidden="true">
+        <use xlink:href="#icon-tongjifenxi-xiangmubiaogetongji"></use>
+      </svg>
+      <router-link to="/labels" class="item">统计</router-link>
+    </div>
+  </nav>
+</template>
+
+<script lang="ts">
+export default {
+  name: "Nav"
+};
+</script>
+
+<style lang="scss" scoped>
+.icon {
+  width: 32px;
+  height: 32px;
+  vertical-align: -0.15em;
+  fill: currentColor;
+  overflow: hidden;
+}
+ nav {
+   display: flex;
+   font-size: 12px;
+   box-shadow: 0 0 3px rgba(0, 0, 0, 0.25);
+   div {
+     display: flex;
+     flex-direction: column;
+     justify-content: center;
+     align-items: center;
+     flex: 1;
+   }
+ }
+</style>
