e8fabc31bb23e86932c7467a42795c8ca48efb61
xiaohh <1296522554@qq.com>
2021-10-18 18:47:06 +0800

    完成了数字板的页面和功能开发

--- src/components/money/NumberPad.vue
+++ src/components/money/NumberPad.vue
@@ -2,20 +2,20 @@
   <div class="numberPad">
     <div class="output">{{output}}</div>
     <div class="buttons">
-      <button>1</button>
-      <button>2</button>
-      <button>3</button>
-      <button>删除</button>
-      <button>4</button>
-      <button>5</button>
-      <button>6</button>
-      <button>清空</button>
-      <button>7</button>
-      <button>8</button>
-      <button>9</button>
-      <button>OK</button>
-      <button>0</button>
-      <button>.</button>
+      <button @click="inputContent">1</button>
+      <button @click="inputContent">2</button>
+      <button @click="inputContent">3</button>
+      <button @click="remove">删除</button>
+      <button @click="inputContent">4</button>
+      <button @click="inputContent">5</button>
+      <button @click="inputContent">6</button>
+      <button @click="clear">清空</button>
+      <button @click="inputContent">7</button>
+      <button @click="inputContent">8</button>
+      <button @click="inputContent">9</button>
+      <button class="ok">OK</button>
+      <button @click="inputContent">0</button>
+      <button @click="inputContent" class="zero">.</button>
     </div>
   </div>
 </template>
@@ -26,6 +26,44 @@ import {Component} from 'vue-property-decorator';
 @Component
 export default class NumberPad extends Vue {
   output = '0';
+  // inputContent 考虑问题
+  // 1. 输入框不能超过16个字符
+  // 2. 第一次的0后面不能再在后面添加0
+  // 3. 输入框里不能同时出现2个点
+  inputContent(event:MouseEvent){
+    const button = event.target as HTMLButtonElement;
+    const input = button.textContent;
+
+    if (this.output.length === 16) {return}
+    if(this.output === '0'){
+      if ('0123456789'.indexOf(input as string) >= 0) {
+        this.output= input;
+      } else {
+        this.output += input;
+      }
+      return;
+    }
+    if (this.output.indexOf('.') >= 0 && input === '.') {return;}
+    this.output += input;
+  }
+
+  // remove 考虑问题
+  // 1. 默认数字为 0
+  // 2. 长度为1时说明用户输入值了
+  remove() {
+    if (this.output.length === 1) {
+      this.output = '0';
+    } else {
+      this.output = this.output.slice(0, -1);
+    }
+  }
+
+  // 直接将当前输入框值变为0
+  clear(){
+    this.output = '0';
+  }
+
+
 }
 </script>
 
