d0a9a42274331b31bedc8d45a903f393b1963c62
xiaohh <1296522554@qq.com>
2021-10-25 14:48:37 +0800

    将 tag 的 string 改为 {id, name}

--- src/views/Labels.vue
+++ src/views/Labels.vue
@@ -1,8 +1,8 @@
 <template>
   <Layout>
     <ol class="tags">
-      <li v-for="tag in tags" :key="tag">
-        <span>{{ tag }}</span>
+      <li v-for="tag in tags" :key="tag.id">
+        <span>{{ tag.name }}</span>
       </li>
     </ol>
     <div class="createTag-wrapper">
